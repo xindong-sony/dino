4e74a3d26e0e537900ef7e0077d3483f1f1106dd
Ubuntu <ubuntu@ip-10-4-19-86.ap-northeast-1.compute.internal>
2023-09-12 23:10:27 +0000

    update

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 NODE_RANK=$1
 export NCCL_SOCKET_IFNAME=ens3
-python -m torch.distributed.launch --nproc-per-node=4 \ 
-           --nnodes=2 --node-rank=$NODE_RANK --master-addr="10.4.19.86" \ 
-           --master-port=1234 main_dino.py --arch vit_small \ 
-           --data_path /home/ubuntu/imagenet_dataset/imagenet/train \ 
+python -m torch.distributed.launch --nproc_per_node=4 \
+           --nnodes=2 --node_rank=$NODE_RANK --master_addr="10.4.19.86" \
+           --master_port=1234 main_dino.py --arch vit_small \
+           --data_path /home/ubuntu/imagenet_dataset/imagenet/train \
            --output_dir /home/ubuntu/
