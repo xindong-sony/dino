3dfe955760d38ec8c3659362531468dbaad6be76
Ubuntu <ubuntu@ip-10-4-27-83.ap-northeast-1.compute.internal>
2023-09-12 23:15:08 +0000

    add timm

--- prepare.sh
+++ prepare.sh
@@ -6,7 +6,7 @@ which pip
 
 pip install torch==1.7.1+cu110 torchvision==0.8.2+cu110 torchaudio==0.7.2 -f https://download.pytorch.org/whl/torch_stable.html
 
-pip install wandb submitit
+pip install wandb submitit timm
 sudo apt install net-tools
 ifconfig 
 export NCCL_SOCKET_IFNAME=ens3
