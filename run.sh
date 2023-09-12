7e38629234ae5f3b340a72b3fcf1f8518d4b9b36
Ubuntu <ubuntu@ip-10-4-19-86.ap-northeast-1.compute.internal>
2023-09-12 23:31:13 +0000

    update

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 NODE_RANK=$1
 export NCCL_SOCKET_IFNAME=ens3
+export MASTER_ADDR=10.4.19.86
+export MASTER_PORT=1234
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+
 python -m torch.distributed.launch --nproc_per_node=4 \
            --nnodes=2 --node_rank=$NODE_RANK --master_addr="10.4.19.86" \
            --master_port=1234 main_dino.py --arch vit_small \
