8c6c95c908b4a80375b66664f36183986f0cb284
Ubuntu <ubuntu@ip-10-4-19-86.ap-northeast-1.compute.internal>
2023-09-23 22:50:19 +0000

    a

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ export NCCL_SOCKET_IFNAME=ens3
 echo $NODE_RANK
 
 python -m torch.distributed.launch --nproc_per_node=4 \
-           --nnodes=2 --node_rank=$NODE_RANK --master_addr="10.4.19.86" \
+           --nnodes=2 --node_rank=$NODE_RANK --master_addr="10.4.27.83" \
            --master_port=12345 main_dino.py --arch vit_small \
            --data_path /home/ubuntu/firstbatch_syn/ \
            --output_dir /home/ubuntu/dino/on_firstbatch_syn \
