80949f2626ee600214dc9347a13f912826ea0ee6
Ubuntu <ubuntu@ip-10-4-27-83.ap-northeast-1.compute.internal>
2023-09-25 17:16:02 +0000

    a

--- run.sh
+++ run.sh
@@ -9,8 +9,9 @@ echo $NODE_RANK
 
 python -m torch.distributed.launch --nproc_per_node=4 \
            --nnodes=2 --node_rank=$NODE_RANK --master_addr="10.4.27.83" \
-           --master_port=12345 main_dino.py --arch vit_small \
+           --master_port=12345 main_dino.py --arch swin_s \
            --data_path /home/ubuntu/firstbatch_syn/ \
+           --num_workers 6 \
            --output_dir /home/ubuntu/dino/on_firstbatch_syn \
            --dist_url tcp://10.4.27.83:12345
 
